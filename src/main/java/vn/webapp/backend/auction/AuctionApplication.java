a2265bb667ee9cc85663e5ff2eeeacf16d634c61
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-05-16 23:02:49 +0700

    init project

---
+++ src/main/java/vn/webapp/backend/auction/AuctionApplication.java
@@ -0,0 +1,13 @@
+package vn.webapp.backend.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/webapp/backend/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.webapp.backend.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
