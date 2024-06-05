01bf6a595b5ab6bfb2c214509c6759d45b5f5288
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-06-05 23:43:06 +0700

    test user service

---
+++ src/test/java/vn/webapp/backend/auction/UserServiceTest.java
@@ -0,0 +1,66 @@
+package vn.webapp.backend.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import vn.webapp.backend.auction.exception.ResourceNotFoundException;
+import vn.webapp.backend.auction.model.User;
+import vn.webapp.backend.auction.service.UserService;
+
+import static org.testng.Assert.*;
+
+
+
+@SpringBootTest
+public class UserServiceTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testGetUserByUsernameReturnsWell() {
+        // Expected
+        String username = "phuuthanh2003";
+        // Act
+        User user = userService.getUserByUsername(username);
+        // Assert
+        assertNotNull(user);
+        assertEquals(user.getUsername(), username);
+    }
+
+    @Test
+    public void testGetUserByIdReturnsWell() {
+        // Expected
+        Integer id = 1;
+
+        // Act
+        User user = userService.getUserById(id);
+
+        // Assert
+        assertNotNull(user);
+        assertEquals(user.getId(), id);
+    }
+
+    @Test
+    public void testGetUserByIdReturnsNull() {
+        Integer nonExistId = 99;
+
+        assertThrows(ResourceNotFoundException.class, () -> userService.getUserById(nonExistId));
+    }
+
+    @Test
+    public void testGetUserByUsernameReturnsNull() {
+        String nonExistUsername = "null";
+
+        assertThrows(ResourceNotFoundException.class, () -> userService.getUserByUsername(nonExistUsername));
+    }
+
+    @Test
+    public void testGetUserByEmailReturnsNull() {
+        String nonExistEmail = "null";
+
+        assertThrows(ResourceNotFoundException.class, () -> userService.getUserByEmail(nonExistEmail));
+    }
+
+}
