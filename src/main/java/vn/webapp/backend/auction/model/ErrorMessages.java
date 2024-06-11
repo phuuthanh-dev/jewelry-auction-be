143bb384e3301fba6985d1a43c4ed08fc52bfd7b
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-06-12 01:04:58 +0700

    feat: create transaction for winner after auction finished

--- src/main/java/vn/webapp/backend/auction/model/ErrorMessages.java
+++ src/main/java/vn/webapp/backend/auction/model/ErrorMessages.java
@@ -3,4 +3,5 @@ package vn.webapp.backend.auction.model;
 public class ErrorMessages {
     public static final String USER_NOT_FOUND = "Không tìm thấy người dùng.";
     public static final String AUCTION_NOT_FOUND = "Không tìm thấy phiên đấu giá.";
+    public static final String USER_WINNER_NOT_FOUND = "không tìm thấy người chiến thắng.";
 }
